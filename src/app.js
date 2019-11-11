a3afbdd9a07a74c7cdeee8e436b10aefcb624a5e
yanjian <mickey0320@126.com>
2019-11-11 23:38:06 +0800

    搭建环境,编写一个简单的 button 单文件组件来测试一下

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+
+import Button from './button'
+
+Vue.component('y-button',Button)
+
+new Vue({
+    el: '#app'
+})
---
+++ src/button.vue
@@ -0,0 +1,13 @@
+<template>
+    <button class="y-button">button</button>
+</template>
+<script>
+    export default {}
+</script>
+<style lang="scss">
+   .y-button{
+      &:hover{
+
+      }
+   }
+</style>
