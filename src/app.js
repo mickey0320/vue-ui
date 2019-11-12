ebc65572f896bcbaff059a41f9d059458ab3936e
yanjian <mickey0320@126.com>
2019-11-12 23:35:50 +0800

    完成button-group

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 
 import Button from './button'
+import ButtonGroup from './button-group'
 
 Vue.component('y-button',Button)
+Vue.component('y-button-group',ButtonGroup)
 
 new Vue({
     el: '#app'
---
+++ src/button-group.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="y-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style lang="scss">
+    .y-button-group{
+        display: inline-flex;
+        > .y-button {
+            &:not(:first-child){
+                margin-left: -1px;
+            }
+            border-radius: 0;
+            &:first-child{
+                border-top-left-radius: var(--button-radius);
+                border-bottom-left-radius: var(--button-radius);
+            }
+            &:last-child{
+                border-top-right-radius: var(--button-radius);
+                border-bottom-right-radius: var(--button-radius);
+            }
+            &:hover{
+                position: relative;
+                z-index: 1;
+            }
+        }
+    }
+</style>
