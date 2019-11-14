dd4a734cf7bbaf0f731d800185f10fb1c97f5827
yanjian <yanjian92@126.com>
2019-11-14 10:10:27 +0800

    input基本样式

---
+++ src/input.vue
@@ -0,0 +1,78 @@
+<template>
+    <div class="wrapper" :class="{error:error}">
+        <input type="text" :value="value" :disabled="disabled" :readonly="readonly" />
+        <template v-if="error">
+            <icon name="settings" class="icon-error"></icon>
+            <span class="icon-msg">{{error}}</span>
+        </template>
+    </div>
+</template>
+
+<script>
+    import Icon from './icon'
+    export default {
+        name: "YInput",
+        components:{icon: Icon},
+        props:{
+            value: {
+                type: String,
+            },
+            disabled:{
+                type: Boolean,
+                default: false,
+            },
+            readonly:{
+                type: Boolean,
+                default: false,
+            },
+            error:{
+                type: String
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    $font-size: 12px;
+    $height: 32px;
+    $border-color: #999;
+    $border-color-hover: #666;
+    $border-radius: 4px;
+    $border-shadow-color: rgba(0,0,0,.5);
+    $red: #F1453D;
+    .wrapper{
+        font-size: $font-size;
+        display: inline-flex;
+        align-items: center;
+        > input{
+            height: 32px;
+            border: 1px solid $border-color;
+            border-radius: $border-radius;
+            padding: 0 8px;
+            font-size: inherit;
+            &:hover{
+                border-color: $border-color-hover;
+            }
+            &:focus{
+                box-shadow: inset 0 1px 3px $border-shadow-color;
+                outline: none;
+            }
+            &[disabled],&[readonly]{
+                border-color: #ccc;
+                color: #ccc;
+                cursor: not-allowed;
+            }
+        }
+        &.error{
+            > input{
+                border-color: $red;
+            }
+            .icon-error{
+                fill: $red;
+            }
+            .icon-msg{
+                color: $red;
+            }
+        }
+    }
+</style>
