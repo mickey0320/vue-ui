38c080b829a7898903cd5303c4c19a789a98fd36
yanjian <yanjian92@126.com>
2019-11-16 10:37:20 +0800

    完成toast动画，样式全部完成

--- src/plugin.js
+++ src/plugin.js
@@ -1,15 +1,26 @@
 import Toast from './toast'
 
+function createToast(Vue, message, options){
+    const Constructor = Vue.extend(Toast)
+    const vm = new Constructor({
+        propsData: options
+    })
+    vm.$slots.default = [message]
+    vm.$mount()
+    document.body.appendChild(vm.$el)
+
+    return vm
+}
+
+let currentToast = null
+
 export default {
     install(Vue){
         Vue.prototype.$toast = function (message, options) {
-            const Constructor = Vue.extend(Toast)
-            const vm = new Constructor({
-                propsData: options
-            })
-            vm.$slots.default = [message]
-            vm.$mount()
-            document.body.appendChild(vm.$el)
+            if(currentToast){
+                currentToast.close()
+            }
+            currentToast = createToast(Vue, message, options)
         }
     }
 }
--- src/toast.vue
+++ src/toast.vue
@@ -1,9 +1,12 @@
 <template>
-   <div class="toast" :class="toastPosition">
-       <span class="text" v-if="!enableHtml"><slot></slot></span>
-       <div v-else v-html="$slots.default[0]"></div>
-       <span class="close" @click="onClose">{{closeButton.text}}</span>
-   </div>
+    <div class="toast-wrapper" :class="toastPosition">
+        <div class="toast">
+            <span class="text" v-if="!enableHtml"><slot></slot></span>
+            <div v-else v-html="$slots.default[0]"></div>
+            <span class="close" @click="onClose">{{closeButton.text}}</span>
+        </div>
+    </div>
+
 </template>
 
 <script>
@@ -43,7 +46,7 @@
         },
         computed:{
             toastPosition(){
-                return `toast-${this.position}`
+                return `toast-wrapper-${this.position}`
             }
         },
         mounted() {
@@ -67,28 +70,64 @@
 </script>
 
 <style lang="scss" scoped>
+    @keyframes fade-in {
+        0%{opacity: 0;}
+        100%{opacity: 1;}
+    }
+    @keyframes fade-in-top {
+        0%{opacity: 0; transform: translateY(-100%);}
+        100%{opacity: 1; transform: translateY(0);}
+    }
+    @keyframes fade-in-bottom {
+        0%{opacity: 0; transform: translateY(100%);}
+        100%{opacity: 1; transform: translateY(0%);}
+    }
     $font-size: 14px;
     $toast-height: 40px;
     $toast-width: 320px;
     $toast-border-radius: 4px;
     $toast-bg: rgba(0,0,0,.75);
+    .toast-wrapper{
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+        &.toast-wrapper-top{
+            top: 0;
+            > .toast{
+                animation: fade-in-top 1s;
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+            }
+        }
+        &.toast-wrapper-middle{
+            top: 50%;
+            transform: translate(-50%,-50%);
+        }
+        &.toast-wrapper-bottom{
+            bottom: 0;
+            > .toast{
+                animation: fade-in-bottom 1s;
+                border-bottom-left-radius: 0;
+                border-bottom-right-radius: 0;
+            }
+        }
+    }
+
     .toast{
+        animation: fade-in 1s;
         font-size: $font-size;
-        min-height: $toast-height;
+        color: white;
         max-width: $toast-width;
         line-height: 1.8;
         background: $toast-bg;
-        position: absolute;
-        left: 50%;
         display: flex;
-        color: white;
+        padding: 0 16px;
         border-radius: $toast-border-radius;
         box-shadow: 0 0 3px 0 rgba(0,0,0,.5);
-        padding: 0 16px;
         .text{
             display: flex;
             align-items: center;
-            padding: 8px 0;
+            padding: 4px 0;
         }
         .close{
             position: relative;
@@ -106,19 +145,6 @@
                 bottom: 0;
                 background-color: #666;
             }
-        }
-        &.toast-top{
-            top: 0;
-            transform: translateX(-50%);
-        }
-        &.toast-middle{
-            top: 50%;
-            
-            transform: (-50%,-50%);
-        }
-        &.toast-bottom{
-            bottom: 0;
-            transform: translateX(-50%);
         }
     }
 </style>
