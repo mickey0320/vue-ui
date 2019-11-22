a28ce7cbc6243c4d5beeee6fa5761eb12effa044
yanjian <yanjian92@126.com>
2019-11-22 15:20:19 +0800

    deploy

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    base: 'vue-ui',
+    title: 'vue组件库',
+    description: '一个用vue写的ui组件库',
+    themeConfig: {
+        sidebar: [
+            {title:'入门', children: ['/install/','/get-started/']},
+            {title:'组件'},
+        ]
+    }
+}
